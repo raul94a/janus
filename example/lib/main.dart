b90000c5ef04740d9d36b4ec6e28a2cfcee8fa39
raul94a <raul200794@gmail.com>
2022-10-28 12:59:53 +0200

    Change to version 0.1.2

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,11 @@
-import 'package:example/src/portunus.dart';
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+
+import 'dart:convert';
+import 'dart:developer';
+
+
 import 'package:flutter/material.dart';
+import 'package:janus/janus.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,15 +20,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+        
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -51,44 +49,44 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
-  void _incrementCounter() {
+  void _incrementCounter() async{
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
+      
       _counter++;
+    });
+    final Counter counter = Counter(number: _counter);
+    final counterStorage = CounterStorage();
+    await counterStorage.save(counter.toMap());
+
+  }
+
+  void getCounter()async{
+    final counterStorage = CounterStorage();
+    final counter = Counter.fromMap(await counterStorage.load());
+    setState(() {
+      _counter = counter.number;
     });
   }
 
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getCounter();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    
+   
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+  
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+       
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
+        
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
@@ -105,7 +103,31 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ), 
     );
   }
+}
+
+class Counter {
+  final int number;
+  const Counter({
+    required this.number,
+  });
+
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'number': number,
+    };
+  }
+
+  factory Counter.fromMap(Map<String, dynamic> map) {
+    return Counter(
+      number: map['number'] ?? 0,
+    );
+  }
+}
+
+class CounterStorage extends Janus{
+  CounterStorage() : super(cypher: true);
 }
